3ab982fbdaa99ef784f4d12f737acf2d0fdc3811
Fabiola Paz <fabiihpaaz@gmail.com>
2022-08-24 21:32:48 -0300

    Nota de Alunos

---
+++ index.js
@@ -0,0 +1,15 @@
+let prompt = require('prompt-sync')();
+
+
+let nota1 = parseFloat(prompt("Digite sua primeira nota? "));
+nota1 += parseFloat(prompt("Digite sua segunda nota? "));
+nota1 += parseFloat(prompt("Digite sua terceira nota? "));
+let media = (nota1)/3
+
+media >= 7 ? console.log("APROVADO") : console.log("REPROVADO")
+
+let nota2 = parseFloat(prompt("Digite sua primeira nota: "));
+nota2 += parseFloat(prompt("Digite sua segunda nota: "));
+
+let media2 = 21-nota2
+console.log(media2)
